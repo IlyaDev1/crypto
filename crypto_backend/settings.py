afa170f26684e6923f5ab5f5473ce2af8b302672
IliDevia <neiluamaibi@mail.ru>
2024-10-28 20:32:06 +0300

    cringe

--- crypto_backend/settings.py
+++ crypto_backend/settings.py
@@ -15,7 +15,7 @@ SECRET_KEY = 'django-insecure-5di-moy=0c5l)gps+q+$^w$owpyck&&(hz=55*r3h2z02ze18k
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
